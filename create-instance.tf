3cf29b0cc7893a4b50a84e1dd36f4afd8fde7dd0
tcosta <tcosta@avenuecode.com>
2019-05-22 10:58:05 -0300

    msg

--- create-instance.tf
+++ create-instance.tf
@@ -17,7 +17,7 @@ resource "google_compute_instance" "default" {
     }
   }
 
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from Terraform on Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html"
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
 
     // Apply the firewall rule to allow external IPs to access this instance
     tags = ["http-server"]
